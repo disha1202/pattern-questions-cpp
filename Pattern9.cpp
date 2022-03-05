235ddd8398b36baaabb8d39ba4a2894e3a9c75ff
disha <dishatalreja1202@gmail.com>
2022-03-05 23:43:14 +0530

    Implemented: logic to generate 4 different kinds of pattern

---
+++ Pattern9.cpp
@@ -0,0 +1,21 @@
+// 1
+// 2 3
+// 3 4 5
+// 4 5 6 7
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=i){
+            cout<<i+j-1<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
